9829a50ac14af482382b58bacda5c5b77db25227
Albertaugustine <yesuarielalbert@gmail.com>
2020-07-07 00:16:36 +0530

    Update and rename add_data.php to putdata.php

--- api/putdata.php
+++ api/putdata.php
@@ -1,16 +1,25 @@
 <?php
 require '../dbconfig.php';
 
-//http://localhost/internship/api/add_data.php
-/*[{
+//http://localhost/internship/api/putdata.php
+
+/*
+Type this json in body. It will be added to the table:
+[{
 "Name":"albert"
-}]*/
+}]
+*/
+
+//Output will be:
+//albert
 
 $put=file_get_contents('php://input');
 $data=(array)json_decode($put, true);
 
-print_r($data[0]['Name']);
+//print_r($data[0]['name']);
 
 $squery="INSERT INTO user (`Name`) VALUES ('".$data[0]['Name']."')";
 $result=$conn->query($squery);
+print_r($data[0]['Name']);
+
 ?>
